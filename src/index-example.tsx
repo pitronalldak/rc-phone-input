e489037cae5489dd7b00175ed96b62a01ad97903
pitronalldak <ryabinin.p.b@gmail.com>
2018-02-11 19:50:03 +0300

    fix index.tsx

---
+++ src/index-example.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+import RCPhoneNumber from '../src/RCPhoneInput'
+
+const App: React.SFC<any> = (): JSX.Element => (<div style={{width: '25%'}}>
+  <RCPhoneNumber value="+79107409656" withIpLookup />
+</div>)
+
+ReactDOM.render(<App />, document.getElementById('app'))
